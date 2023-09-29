d4ddc899adff3796972e6802cd9ba51007f0a88e
Mia Yv <mia.yv@icloud.com>
2023-09-29 11:04:40 +0800

    fix: not display site description

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const SITE_DOMAIN = "https://swiftui.codes";
 export const SITE_TITLE = "Swift. UI. Code.";
-export const SITE_DESCRIPTION = "Sharing ideas, tips, stories, and playgrounds on SwiftUI";
+export const SITE_DESCRIPTION = "Sharing ideas, inspirations and insights on SwiftUI";
 
 export const GITHUB = "/";
 export const TWITTER = "/";
